d6205ce34f5588d8b66539a62a9e0f3608df3eca
yegor.kariaguine <yegor.kariaguine@chefsplate.com>
2020-05-13 15:08:30 -0400

    eslint added

--- components/date.tsx
+++ components/date.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { parseISO, format } from "date-fns";
 
-export default function Date({ dateString }: { dateString: string }) {
+export default function Date({
+  dateString,
+}: {
+  dateString: string;
+}): JSX.Element {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 }
