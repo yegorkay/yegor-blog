9674d4a9b43a2f260aa9f3453dbffec54b681706
yegor.kariaguine <yegor.kariaguine@chefsplate.com>
2020-05-13 15:35:30 -0400

    some formatting

--- components/date.tsx
+++ components/date.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { parseISO, format } from "date-fns";
 
-export default function Date({
-  dateString,
-}: {
+interface DateProps {
   dateString: string;
-}): JSX.Element {
+}
+
+export default function Date({ dateString }: DateProps): JSX.Element {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -6,30 +6,34 @@ import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
 
-export default function Post({
-  postData,
-}: {
-  postData: {
-    title: string;
-    date: string;
-    contentHtml: string;
-  };
-}) {
-  return (
-    <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
-  );
+interface PostData {
+  title: string;
+  date: string;
+  contentHtml: string;
 }
+
+interface PostProps {
+  postData: PostData;
+}
+
+const Post = ({ postData }: PostProps): JSX.Element => (
+  <Layout>
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
+    <article>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: postData.contentHtml,
+        }}
+      />
+    </article>
+  </Layout>
+);
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
@@ -47,3 +51,5 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     },
   };
 };
+
+export default Post;
